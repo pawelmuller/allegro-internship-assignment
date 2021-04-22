b1413232c1c759bdcad59bc2ff9f8c329dd7f4fa
Paweł Muller <pawelmuller42@gmail.com>
2021-04-23 00:02:54 +0200

    Added import_repositories method in GitHubUser class. It requests list of repositories of given user from GitHub API.

--- GitHubUser.py
+++ GitHubUser.py
@@ -4,7 +4,12 @@ URL = "https://api.github.com"
 
 
 class GitHubUser:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
-        self.repositories = None
-        self.star_count = None
+        self.repositories = self.import_repositories()
+        self.summed_stars_count = None
+
+    def import_repositories(self):
+        repos = requests.get(url=f"{URL}/users/{self.name}/repos").json()
+        return repos
+
