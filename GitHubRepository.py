5ce238eca672e950a0b6b7e082d7b6ef61bbec5e
Paweł Muller <pawelmuller42@gmail.com>
2021-04-22 23:54:37 +0200

    Added extract_name() and extract_stars_count() methods for GitHubRepository class. They return value of appropriate key from repository dict.

--- GitHubRepository.py
+++ GitHubRepository.py
@@ -1,4 +1,12 @@
 class GitHubRepository:
-    def __init__(self):
-        self.name = None
-        self.summed_stars_count = None
+    def __init__(self, raw_repository_json):
+        self.name = self.extract_name(raw_repository_json)
+        self.stars_count = self.extract_stars_count(raw_repository_json)
+
+    @staticmethod
+    def extract_name(raw_repository):
+        return raw_repository["name"]
+
+    @staticmethod
+    def extract_stars_count(raw_repository):
+        return raw_repository["stargazers_count"]
