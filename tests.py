65fb8c08259a8a670a97858bbebbfe2e3dcb0a66
Paweł Muller <pawelmuller42@gmail.com>
2021-04-22 23:59:20 +0200

    Added test for value extraction. Tests GitHubRepository.extract methods.

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+from GitHubRepository import GitHubRepository
 
 client = TestClient(app)
 
@@ -10,3 +11,8 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
 
+
+def test_extraction():
+    test_dict = {"name": "Arthur", "stargazers_count": 15}
+    assert GitHubRepository.extract_name(test_dict) == "Arthur"
+    assert GitHubRepository.extract_stars_count(test_dict) == 15
