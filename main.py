683332f8da278ae370855b60759e52d29ad9eec1
Paweł Muller <pawelmuller42@gmail.com>
2021-04-24 12:58:03 +0200

    Added user validation for /git/users/{username}/paths endpoint.

--- main.py
+++ main.py
@@ -19,11 +19,15 @@ async def get_repos(username: str, response: Response):
         return {"message": "User Not Found"}
 
 
-@app.get("/git/users/{username}/repos")
-async def get_repos(username: str):
+@app.get("/git/users/{username}/repos", status_code=status.HTTP_200_OK)
+async def get_repos(username: str, response: Response):
     user = GitHubUser(username)
-    repositories = user.get_repositories()
-    return repositories
+    if user.is_valid():
+        repositories = user.get_repositories()
+        return repositories
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "User Not Found"}
 
 
 @app.get("/git/users/{username}/stars")
