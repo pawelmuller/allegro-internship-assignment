20a03b8f54461fc63833a8494ededa3aae3a3b1e
Paweł Muller <pawelmuller42@gmail.com>
2021-04-24 11:16:06 +0200

    Unified endpoint paths to be similar with GitHub ones.

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def root():
     return {"message": "Hello world!"}
 
 
-@app.get("/git/{username}/repos")
+@app.get("/git/users/{username}")
+async def get_repos(username: str):
+    user = GitHubUser(username)
+    return user
+
+
+@app.get("/git/users/{username}/repos")
 async def get_repos(username: str):
     user = GitHubUser(username)
     repositories = user.get_repositories()
     return repositories
 
 
-@app.get("/git/{username}/stars")
+@app.get("/git/users/{username}/stars")
 async def get_stars(username: str):
     user = GitHubUser(username)
     stars_count = user.get_total_stars_count()
